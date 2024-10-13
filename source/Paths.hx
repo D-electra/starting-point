3ad989d488bb9a89ceb8326460a3a0c7fac3080c
D-electra <mortirfan@gmail.com>
2024-10-14 09:08:31 +1100

    remove list functions from paths

--- source/Paths.hx
+++ source/Paths.hx
@@ -8,25 +8,13 @@ class Paths {
 		return 'assets/images/$key.png';
 	}
 
-	public static inline function listImages(key:String):Array<String> {
-		return Assets.list(IMAGE).filter((v) -> v.startsWith('assets/images/$key'));
-	}
-
 	public static inline function music(key:String):String {
 		if (key.contains('.')) return 'assets/music/$key';
 		return #if web 'assets/music/$key.mp3' #else 'assets/music/$key.ogg' #end;
 	}
 
-	public static inline function listMusic(key:String):Array<String> {
-		return Assets.list(SOUND).filter((v) -> v.startsWith('assets/sounds/$key'));
-	}
-
 	public static inline function sound(key:String):String {
 		if (key.contains('.')) return 'assets/sounds/$key';
 		return #if web 'assets/sounds/$key.mp3' #else 'assets/sounds/$key.ogg' #end;
-	}
-
-	public static inline function listSounds(key:String):Array<String> {
-		return Assets.list(SOUND).filter((v) -> v.startsWith('assets/sounds/$key'));
 	}
 }
